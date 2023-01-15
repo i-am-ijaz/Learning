4586027e0c0771ffd31bec442d35a1fd16ccbf99
ijazsharif <ijazsharif34@gmail.com>
2023-01-15 20:25:43 +0500

    sample app created

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,16 +3,21 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapplication.ui.theme.MyApplicationTheme
 import androidx.compose.ui.unit.dp
+import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,32 +32,93 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
-    Surface(
-        color = MaterialTheme.colors.background
-    ) {
-        Greeting("Android")
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinuePressedCallback = { shouldShowOnboarding = false })
+    } else {
+        GreetingsScreen()
+    }
+}
+
+@Composable
+fun GreetingsScreen(
+    names: List<String> = List(1000) { "$it" }
+) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
+
+    var isExpanded by remember { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(
+        if (isExpanded) 48.dp else 0.dp, animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy, stiffness = Spring.StiffnessLow
+        )
+    )
     Surface(
-        color = MaterialTheme.colors.primary
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
     ) {
         Row(
+            modifier = Modifier.padding(24.dp)
         ) {
-            Text(
-                text = "Hello $name!",
-                modifier = Modifier.padding(24.dp),
-            )
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+            ) {
+                Text(text = "Hello,")
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.h6.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    ),
+                )
+            }
+            OutlinedButton(onClick = { isExpanded = !isExpanded }) {
+                Text(text = if (isExpanded) "Show less" else "Show more")
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun OnboardingScreen(
+    modifier: Modifier = Modifier, onContinuePressedCallback: () -> Unit
+) {
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp), onClick = onContinuePressedCallback
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    MyApplicationTheme {
+        OnboardingScreen(onContinuePressedCallback = {})
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        MyApp()
+        GreetingsScreen()
     }
 }
